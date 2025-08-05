4108d42fcb3a4090acf9d596e541ff7acedfe867
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2025-08-05 21:57:38 +0530

    fix: added projects pics

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,6 @@ export default function Home() {
     <Departments />
     <Members />
     <Projects/>
-    <Events/>
     <Footer/>
     </>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
   let proj = [
     {
       name: "Line Follower Robot",
-      url: "/assets/projects/project_name.jpg",
+      url: "/assets/projects/linefollower.jpg",
       desc: "A line follower robot is an autonomous robot that follows a specific path, usually a black line on a white surface. It uses infrared sensors to detect the line and adjust its movement accordingly. The robot continuously scans the path and makes real-time decisions to stay on track. It's often used to demonstrate concepts of automation, control systems, and robotics. consectetur, adipisicing elit. Nesciunt quis quam quo! Cupiditate aliquid necessitatibus quasi voluptates repellendus molestiae mollitia magnam possimus officiis."
     },
     {
